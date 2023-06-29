331346f2a5ea3f8b85291c836c572399e54f871a
Kamila Kazimierska <kamka@orsted.com>
2023-06-29 14:41:59 +0200

    Feat: testy

--- test_na_obecnosc.py
+++ test_na_obecnosc.py
@@ -1,2 +1,13 @@
- from wczytaj_dane import imie
- imie == "Dorota"
+from wczytaj_dane import imie
+#  imie == "Dorota"
+
+# Zadanie: Napisz funkcję test_imie_Dorota_jest_zapisane_w_wczytaj_dane.
+def test_imie_Dorota_jest_zapisane_w_wczytaj_dane():
+    assert imie == "Dorota"
+
+# test_wczytaj_imiona_z_pliku
+from wczytaj_dane import wczytaj_do_listy_ze_sciezki
+
+def test_wczytaj_imiona_z_pliku():
+    wczytana_lista = wczytaj_do_listy_ze_sciezki("lista_imion.txt")
+    assert len(wczytana_lista) == 5
